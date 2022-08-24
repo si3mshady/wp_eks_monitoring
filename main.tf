9803a773c3d790d2ba96b929a6c802020626a6ae
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-08-24 03:53:35 -0500

    deploying eks cluster

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "vpc" {
 
 module "eks" {
   source             = "./eks"
-  public_eks_subnets = [module.vpc.public_subnets_cidr_blocks]
+  public_eks_subnets = [module.vpc.public_subnets]
   vpc_id             = module.vpc.vpc_id
 
 }

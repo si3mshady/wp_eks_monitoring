4428730cbe3d2b418fefbde739d6580586a8dd66
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-08-24 03:26:26 -0500

    deploying eks cluster

--- main.tf
+++ main.tf
@@ -6,20 +6,21 @@ module "vpc" {
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102te.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
 output "metavpc" { value = module.vpc }
 
-module eks  {
+module "eks" {
 
-public_eks_subnets = output.metavpc.public_subnets 
-vpc_id = output.metavpc.vpc_id
+  source             = "./eks"
+  public_eks_subnets = output.metavpc.public_subnets
+  vpc_id             = output.metavpc.vpc_id
 
 }
 
--- providers.tf
+++ providers.tf
@@ -3,14 +3,14 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-    #   version = "3.26.0"
+      source = "hashicorp/aws"
+      #   version = "3.26.0"
     }
-    
+
   }
 
 
- 
+
 }
 
 provider "aws" {

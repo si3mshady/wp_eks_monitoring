2c40da3fa893c6a63e30cbee51f9e878963c220b
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-08-24 02:26:15 -0500

    updated workflow

--- providers.tf
+++ providers.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+    #   version = "3.26.0"
     }
     
   }
-  required_version = ">= 1.1.0"
+#   required_version = ">= 1.1.0"
 
  
 }

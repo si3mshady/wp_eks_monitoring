49615b41bff2105c063b6f9a1a5f9bd9f81e6337
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-08-24 03:45:58 -0500

    deploying eks cluster

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-
-  public_subnets  = [for i in range(5, 255, 2) : cidrsubnet(var.vpc_cidr, 10, i)]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
     Terraform   = "true"
@@ -17,7 +17,7 @@ module "vpc" {
 module "eks" {
 
   source             = "./eks"
-  public_eks_subnets = [for cidr in module.vpc.public_subnets_cidr_blocks: cidr.public_subnets_cidr_blocks ]
+  public_eks_subnets = [module.vpc.public_subnets_cidr_blocks]
   vpc_id             = module.vpc.vpc_id
 
 }
